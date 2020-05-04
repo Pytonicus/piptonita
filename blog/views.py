d6f509f659a6bf49abeba20da3263cffd3a91202
Guillermo Granados Gomez <guillermogranadosgomez@gmail.com>
2020-05-04 23:44:04 +0200

    Revision de cursos en admin

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from .models import Entrada
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
 
 class EntradaListView(ListView):
     model = Entrada
-    paginate_by = 2
+    paginate_by = 5
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["url"] = 'blog'
         return context
-    
+
+class EntradaDetailView(DetailView):
+    model = Entrada
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['url'] = 'blog'
+        return context
--- cursos/admin.py
+++ cursos/admin.py
@@ -1,5 +1,13 @@
 from django.contrib import admin
 from .models import Curso, Alumno
 
-admin.site.register(Curso)
-admin.site.register(Alumno)
+class CursoAdmin(admin.ModelAdmin):
+    readonly_fields = ('fecha_entrada',)
+    list_display = ('nombre_curso', 'horario', 'precio', 'fecha_inicio')
+
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('nombre_alumno', 'apellidos', 'telefono', 'email', 'curso_registrado', 'fecha_inscripcion', 'contactado', 'comenzado_curso', 'segunda_sesion_prueba', 'pago_realizado', 'finalizado_curso')
+
+admin.site.register(Curso, CursoAdmin)
+admin.site.register(Alumno, AlumnoAdmin)
+
